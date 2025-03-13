433a9deaec22dc860f0bfbec4598c960cbabb631
Your Name <you@example.com>
2025-03-13 18:45:07 -0500

    proxy fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      'api': {
+      '/api': {
         target: 'https://jpems-api.vercel.app',
         changeOrigin: true
       },
